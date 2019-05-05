048ab556b7562831002b0bcf13d5cbbaa7a6e902
triplexone <hrvoje.viskovic@gmail.com>
2019-05-05 14:46:17 +0200

    alfonvi i velonci u ruby-u

---
+++ alfonci-velonci.rb
@@ -0,0 +1,24 @@
+alfonci = 5000000
+velonci = 9000000
+alfonci_rast = 0.06
+rat = 500000
+godine = 0
+
+while alfonci < velonci do
+  godine += 1
+  alfonci += alfonci.to_i * alfonci_rast
+
+  if godine % 4 == 0
+    velonci_rast = 0.05
+    velonci += ((velonci.to_i * velonci_rast)-rat)
+  elsif
+    velonci_rast = 0.02
+    velonci += velonci.to_i * velonci_rast
+  end
+
+    puts "Populacija #{godine}. godine: "
+    puts "\tAlfonaca ima: #{alfonci.to_i}"
+    puts "\tVelonaca ima: #{velonci.to_i}"
+end
+
+puts "Za #{godine} godina bit će više Alfonaca od Velonaca."
